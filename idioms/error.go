34140606e53e662a74c2368e34a17fd610288294
Charles Iliya Krempeaux <iamreiver@gmail.com>
2021-01-16 10:32:50 -0800

    docs

--- idioms/error.go
+++ idioms/error.go
@@ -7,6 +7,15 @@ import (
 
 const errorPrefix = "idiom"
 
+// Error returns an error whose message is prefixed with "idiom: ".
+//
+// For example with:…
+//
+//	err := idioms.Error("something bad happened")
+//
+// … the errors message would be:
+//
+//	idiom: something bad happened
 func Error(a ...interface{}) error {
 	prefix := fmt.Sprintf("%s:", errorPrefix)
 
@@ -17,6 +26,17 @@ func Error(a ...interface{}) error {
 	return errors.New(text)
 }
 
+// Errorf returns an error whose message is prefixed with "idiom: ".
+//
+// For example with:…
+//
+//	var name = "Joe Blow"
+//	
+//	err := idioms.Errorf("%s is not allowed here", name)
+//
+// … the errors message would be:
+//
+//	idiom: Joe Blow is not allowed here
 func Errorf(format string, a ...interface{}) error {
 	format = fmt.Sprintf("%s: %s", errorPrefix, format)
 
