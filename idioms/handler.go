29775dc269a6b31e33cf0374e63099284d4418ac
Charles Iliya Krempeaux <iamreiver@gmail.com>
2021-01-16 10:40:19 -0800

    docs

--- idioms/handler.go
+++ idioms/handler.go
@@ -1,5 +1,9 @@
 package idioms
 
+// Handler represents the implementation side — what actually does the work — of a specific idiom command.
+//
+// NOTE that the name of the idiom command is not part of the Handler. But instead the name of the command
+// is set when the Handler is registered.
 type Handler interface {
 	Run(...string) (string, error)
 }
