54eddf32394ab327c840ad2b18d772959dce0d12
Charles Iliya Krempeaux <iamreiver@gmail.com>
2021-01-16 11:49:45 -0800

    idiom_string.Type

--- idioms/handlerfunc.go
+++ idioms/handlerfunc.go
@@ -1,12 +1,16 @@
 package idioms
 
+import (
+	"../string"
+)
+
 //  HandlerFunc is an adaptor that turns a func with signature:…
 //
-//	func(...string)(string, error)
+//	func(...idiom_string.Type)idiom_string.Type
 //
 // … into a idioms.Handler
-type HandlerFunc func(...string)(string, error)
+type HandlerFunc func(...idiom_string.Type)idiom_string.Type
 
-func (fn HandlerFunc) Run(parameters ...string) (string, error) {
+func (fn HandlerFunc) Run(parameters ...idiom_string.Type) idiom_string.Type {
 	return fn(parameters...)
 }
