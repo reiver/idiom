00ffc8d774c7e43de3770e559cc4877b6f6fb677
Charles Iliya Krempeaux <iamreiver@gmail.com>
2021-01-16 10:45:35 -0800

    docs

--- idioms/handlerfunc.go
+++ idioms/handlerfunc.go
@@ -1,5 +1,10 @@
 package idioms
 
+//  HandlerFunc is an adaptor that turns a func with signature:…
+//
+//	func(...string)(string, error)
+//
+// … into a idioms.Handler
 type HandlerFunc func(...string)(string, error)
 
 func (fn HandlerFunc) Run(parameters ...string) (string, error) {
