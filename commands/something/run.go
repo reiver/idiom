49db4cd16761ac8c25820b43d61e95f52ee162b5
Charles Iliya Krempeaux <iamreiver@gmail.com>
2021-01-20 11:07:26 -0800

    something

--- commands/something/run.go
+++ commands/something/run.go
@@ -1,17 +1,10 @@
 package verboten
 
 import (
-	"../../kernel"
 	"../../string"
-
-	"io"
 )
 
 func Run(parameters ...idiom_string.Type) idiom_string.Type {
-	return run(idiom_kernel.Stem.Stdout(), parameters...)
-}
-
-func run(stdout io.Writer, parameters ...idiom_string.Type) idiom_string.Type {
 
 	if expected, actual := 1, len(parameters); expected != actual {
 		return idiom_string.Errorf("expected %d parameter(s), but actually got %d", expected, actual)
