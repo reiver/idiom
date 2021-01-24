b76701f9708559450439d0b17e21ac5048d0d4a1
Charles Iliya Krempeaux <iamreiver@gmail.com>
2021-01-24 10:20:20 -0800

    tracing

--- kernel/branch_run.go
+++ kernel/branch_run.go
@@ -7,11 +7,14 @@ import (
 
 func (receiver *Branch) Run(command string, parameters ...idiom_string.Type) idiom_string.Type {
 
-	log.Trace("[run] BEGIN")
-	log.Tracef("[run] command = %q", command)
-	log.Tracef("[run] len(parameters) = %d", len(parameters))
-	for i,p := range parameters {
-		log.Tracef("[run]\tparameters[%d] = %#v", i, p)
+	if log.Tracing() {
+		log.Trace("[run] BEGIN")
+		log.Tracef("[run] command = %q", command)
+		log.Tracef("[run] PREVIOUS returned = %#v", receiver.Returned())
+		log.Tracef("[run] len(parameters) = %d", len(parameters))
+		for i,p := range parameters {
+			log.Tracef("[run]\tparameters[%d] = %#v", i, p)
+		}
 	}
 
 	if nil == receiver {
